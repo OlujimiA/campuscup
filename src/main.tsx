c9853c6681230799183f2e87b4d7c4a41974a5ab
OlujimiA <184277082+OlujimiA@users.noreply.github.com>
2026-05-10 13:46:57 +0100

    Edit main

--- src/main.tsx
+++ src/main.tsx
@@ -4,43 +4,15 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import { Authenticator, ThemeProvider } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
-const theme = {
-  name: 'CampusCupTheme',
-  tokens: {
-    components: {
-      button: {
-        primary: {
-          backgroundColor: '#6b21a8',
-          _hover: {
-            backgroundColor: '#581c87',
-          },
-        },
-      },
-
-      tabs: {
-        item: {
-          color: '#6b21a8',
-          _active: {
-            borderColor: '#6b21a8',
-            color: '#6b21a8',
-          },
-        },
-      },
-    },
-  },
-};
 
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Authenticator>
-        <App/>
-      </Authenticator>
-    </ThemeProvider>
+    <Authenticator>
+      <App/>
+    </Authenticator>
   </React.StrictMode>
 );
